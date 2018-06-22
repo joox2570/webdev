f8f333f86a1126ac2464a60c39cda16450899f68
hjjoo <hjjoo@myseneca.ca>
2018-06-22 14:35:35 -0400

    Version 3

--- data-service.js
+++ data-service.js
@@ -31,28 +31,86 @@ module.exports.getAllEmployees = function(){
     });
 };
 
+module.exports.getEmployeesByStatus = function(status){
+    return new Promise(function(resolve, reject){
+        var emp = [];
+        for(var i = 0; i < employees.length; i++){
+            if(employees[i].status == status)
+                emp.push(employees[i]);
+        }
+        if(emp.length == 0)
+            reject('no results returned');
+        else
+            resolve(emp);    
+    })
+}
+
+module.exports.getEmployeesByDepartment = function(department){
+    return new Promise(function(resolve, reject){
+        var emp = [];
+        for(var i = 0; i < employees.length; i++){
+            if(employees[i].department == department)
+                emp.push(employees[i]);
+        }
+        if(emp.length == 0)
+            reject('no results returned');
+        else
+            resolve(emp);
+    })
+}
+
+module.exports.getEmployeesByManager = function(manager){
+    return new Promise(function(resolve, reject){
+        var emp = [];
+        for(var i = 0; i < employees.length; i++){
+            if(employees[i].employeeManagerNum == manager)
+                emp.push(employees[i]);
+        }
+        if(emp.length == 0)
+            reject('no results returned');
+        else
+            resolve(emp);
+    })
+}
+
+module.exports.getEmployeeByNum = function(num){
+    return new Promise(function(resolve, reject){
+        for(var i = 0; i < employees.length; i++){
+            if(employees[i].employeeNum == num)
+                resolve(employees[i]);
+        }
+        reject('no results returned');
+    })
+}
+
 module.exports.getManagers = function(){
     return new Promise(function(resolve, reject){
         var managers = [];
         for(var i = 0; i < employees.length; i++){
-            if(employees[i].isManager == true){
+            if(employees[i].isManager == true)
                 managers.push(employees[i]);
-            }
         }
-        if(managers.length == 0){
+        if(managers.length == 0)
             reject('no results returned');
-        } else{
+        else
             resolve(managers);
-        }
     });
 };
 
 module.exports.getDepartments = function(){
     return new Promise(function(resolve, reject){
-        if(departments.length == 0){
+        if(departments.length == 0)
             reject('no results returned');
-        } else{
+        else
             resolve(departments);
-        }
     });
 };
+
+module.exports.addEmployees = function(employeeData){
+    return new Promise(function(resolve, reject){
+        employees.isManager = undefined ? false : true;
+        employeeData.employeeNum = employees.length + 1;
+        employees.push(employeeData);
+        resolve();
+    })
+}
--- server.js
+++ server.js
@@ -1,14 +1,57 @@
-var express = require("express");
-var HTTP_PORT = process.env.PORT || 8080;
-var app = express();
-var path = require("path");
+/*********************************************************************************
+* WEB322 – Assignment 02
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Hyun Jun Joo  Student ID: 125930172  Date: June 1, 2018
+*
+* Online (Heroku) Link: https://powerful-garden-81674.herokuapp.com/
+*
+********************************************************************************/
+
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const path = require("path");
 var data = require("./data-service.js");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+
+var HTTP_PORT = process.env.PORT || 8080;
 
 function onHttpStart(){
     console.log("Express http server listening on " + HTTP_PORT);
 }
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended : true}));
+
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function(req, file, cb){
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({storage: storage});
+
+app.post("/images/add", upload.single("imageFile"), (req, res) => {
+    res.redirect("/images");
+})
+
+app.post("/employees/add", (req, res) => {
+    data.addEmployees(req.body);
+    res.redirect("/employees");
+})
+
+app.get("/images", function(req, res){
+    fs.readdir(path.join(__dirname,"/public/images/uploaded"), function(err, items){
+        var images = [];
+        images = items;
+        res.json({"images" : images});
+    });
+});
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname,"/views/home.html"));
@@ -18,11 +61,42 @@ app.get("/about", function(req, res){
     res.sendFile(path.join(__dirname,"/views/about.html"));
 });
 
+app.get("/employees/add", function(req, res){
+    res.sendFile(path.join(__dirname,"/views/addEmployee.html"));
+});
+
+app.get("/images/add", function(req, res){
+    res.sendFile(path.join(__dirname,"/views/addImage.html"));
+});
+
 app.get("/employees", function(req, res){
-    data.getAllEmployees()
+    if(req.query.status){
+        data.getEmployeesByStatus(req.query.status)
+        .then(function(employees) {res.json(employees)})
+        .catch(function(rejectMsg) {res.json({message:rejectMsg})});
+    }
+    else if(req.query.department){
+        data.getEmployeesByDepartment(req.query.department)
+        .then(function(employees) {res.json(employees)})
+        .catch(function(rejectMsg) {res.json({message:rejectMsg})});
+    }
+    else if(req.query.manager){
+        data.getEmployeesByManager(req.query.manager)
+        .then(function(employees) {res.json(employees)})
+        .catch(function(rejectMsg) {res.json({message:rejectMsg})});
+    }
+    else {
+        data.getAllEmployees()
+        .then(function(employees) {res.json(employees)})
+        .catch(function(rejectMsg) {res.json({message:rejectMsg})});
+    }
+});
+
+app.get("/employee/:value", function(req,res){
+    data.getEmployeeByNum(req.params.value)
     .then(function(employees) {res.json(employees)})
     .catch(function(rejectMsg) {res.json({message:rejectMsg})});
-});
+})
 
 app.get("/managers", function(req, res){
     data.getManagers()
